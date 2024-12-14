e65f4327eefd718a18cc4461aab7e9122f259f61
Dustin Shahan <echo@mailbox.org>
2024-12-14 08:42:58 +0100

    renamed

--- controller.cpp
+++ controller.cpp
@@ -1,29 +1,67 @@
 #include "include/unitree_legged_sdk/unitree_legged_sdk.h"
-#include <math.h>
 #include <iostream>
-#include <stdio.h>
+#include <math.h>
 #include <stdint.h>
+#include <stdio.h>
+// #include <Eigen/Core>
+// #include <Eigen/Geometry>
 #define BOOST_BIND_GLOBAL_PLACEHOLDERS
 
-using namespace UNITREE_LEGGED_SDK;
+// using namespace UNITREE_LEGGED_SDK;
+#define U0T UNITREE_LEGGED_SDK
 
-class Robot {
+enum class GaitType : uint8_t { Idle, Trot, Climb_stair, Trot_obstacle };
+enum class Robot_Mode : uint8_t {
+  Idle,                    // idle, default stand
+  Force_stand,             // force stand (controlled by dBodyHeight + ypr)
+  Target_velocity_walking, // target velocity walking (controlled by velocity +
+                           // yawSpeed)
+  Target_position_walking_RESERVED, // target position walking (controlled by
+                                    // position + ypr[0]), reserve
+  Path_mode_walking_RESERVED, // path mode walking (reserve for future release),
+                              // reserve
+  Stand_down,
+  Stand_up,
+  Damping_mode,
+  Recovery_stand,
+  Backflip_RESREVE, // backflip, reserve
+  Jump_yaw, // jumpYaw, only left direction. Note, to use this mode, you need to
+            // set
+  Straight_hand //  straightHand. Note, to use this mode, you need to set mode =
+                //  1 first.
+};
+
+class Quadruped {
 public:
-	Robot() : safe(LeggedType::Go1),
-			  udp(HIGHLEVEL, 8090, "192.168.123.161", 8082)
-	{
-		HighCmd cmd = {0};
-		udp.InitCmdData(cmd); 
-	}
+  Quadruped()
+      : safe(UT::LeggedType::Go1),
+        udp(UT::HIGHLEVEL, 8090, "192.168.123.161", 8082) {
+    UT::HighCmd cmd = {0};
+    udp.InitCmdData(cmd);
+  }
 
-	void control_loop();
+  void control_loop();
 
-	Safety safe;
-	UDP udp;
-	HighState state = {0};
-	int motiontime = 0;
-	float dt = 0.002;
+  int motiontime = 0;
+  float dt = 0.002;
+  Robot_Mode mode = Robot_Mode::Force_stand;
+  GaitType gait_type = GaitType::Trot;
+  uint8_t speed_level = 0;
+  float foot_raise_height;
+  float body_height;
 
+  UT::Safety safe;
+  UT::UDP udp;
+  UT::HighState state = {0};
+  UT::LowState low_state = {0};
+
+  // Eigen::Matrix4d go1_config_matrix[4];
+  const double trunk_length = 0.3762 / 2;
+  const double trunk_width = 0.0935 / 2;
+  const double l1 = 0.;
+  const double l2 = 0.080; // hip
+  const double l3 = 0.213; // thigh
+  const double l4 = 0.213; // calf
 };
 
 // void UDPRecv() {
@@ -34,29 +72,31 @@ public:
 // 	udp.Send();
 // }
 //
-void Robot::control_loop() {}
-
+void Quadruped::control_loop() {}
 
 int main(void) {
-	std::cout << "Robot level set to: HIGH" << std::endl
-			  << "WARNING: Make sure the robot is standing on the ground." << std::endl
-			  << "Press Enter to continue..." << std::endl;
-	std::cin.ignore();
+  std::cout << "Robot level set to: HIGH" << std::endl
+            << "WARNING: Make sure the robot is standing on the ground."
+            << std::endl
+            << "Press Enter to continue..." << std::endl;
+  std::cin.ignore();
 
-	Robot robot;
+  Quadruped robot;
 
-	LoopFunc loop_control("control_loop", robot.dt, boost::bind(&Robot::control_loop, &robot));
-	LoopFunc loop_udpSend("udp_send", robot.dt, 3, boost::bind(&UDP::Recv, &robot.udp));
-	LoopFunc loop_udpRecv("udp_recv", robot.dt, 3, boost::bind(&UDP::Send, &robot.udp));
+  UT::LoopFunc loop_control("control_loop", robot.dt,
+                            boost::bind(&Quadruped::control_loop, &robot));
+  UT::LoopFunc loop_udpSend("udp_send", robot.dt, 3,
+                            boost::bind(&UT::UDP::Recv, &robot.udp));
+  UT::LoopFunc loop_udpRecv("udp_recv", robot.dt, 3,
+                            boost::bind(&UT::UDP::Send, &robot.udp));
 
-	loop_udpSend.start();
-	loop_udpRecv.start();
-	loop_control.start();
+  loop_udpSend.start();
+  loop_udpRecv.start();
+  loop_control.start();
 
-	while (1){
-		sleep(10);
-	}
+  while (1) {
+    sleep(10);
+  }
 
-	return 0;
-
-} 
+  return 0;
+}
