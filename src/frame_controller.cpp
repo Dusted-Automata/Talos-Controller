5d1849771c984320e151089d6e63722876bcb949
Dustin Shahan <echo@mailbox.org>
2025-04-07 03:49:43 +0200

    aded frame_controller

---
+++ src/frame_controller.cpp
@@ -0,0 +1,20 @@
+#include "frame_controller.hpp"
+#include "transformations.hpp"
+#include "types.hpp"
+
+void Frame_Controller::move_in_local_frame(Velocity2d &velocity)
+{
+    local_frame.orientation.rotate(Eigen::AngleAxisd((velocity.angular.z()), Vector3d::UnitZ()));
+    local_frame.pos += local_frame.orientation.rotation() * velocity.linear;
+
+    Vector3d linear_vector = wgsned2ecef(velocity.linear, local_frame.origin);
+    global_frame.orientation.rotate(Eigen::AngleAxisd((velocity.angular.z()), Vector3d::UnitZ()));
+    global_frame.pos += linear_vector;
+}
+
+void Frame_Controller::move_in_global_frame(Velocity2d &velocity)
+{
+    local_frame.orientation.rotate(Eigen::AngleAxisd((velocity.angular.z()), Vector3d::UnitZ()));
+    Linear_Velocity local_vel = local_frame.orientation.rotation() * velocity.linear;
+    local_frame.pos += local_vel;
+}
---
+++ src/frame_controller.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include "types.hpp"
+
+struct Frame
+{
+    // Eigen::Quaterniond orientation;
+    Affine3d orientation;
+    Vector3d pos;
+};
+
+struct NED_Frame : public Frame
+{
+    Ecef_Coord origin;
+};
+
+class Frame_Controller
+{
+  public:
+    // Body frame to local_frame to global_frame
+    // Body frame being the Identity, since it is always on top of the robot
+    // local frame being NED
+    // global frame being ecef.
+    NED_Frame local_frame;
+    Frame global_frame;
+
+    void move_in_local_frame(Velocity2d &velocity);
+
+    void move_in_global_frame(Velocity2d &velocity);
+};
