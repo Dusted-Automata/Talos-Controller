949a81721387d178af633d604f1407b8fbcbe089
Dustin Shahan <echo@mailbox.org>
2025-04-07 04:00:12 +0200

    didn't rename to linear_controller correctly

--- src/robot.cpp
+++ src/robot.cpp
@@ -10,7 +10,7 @@ void Robot::control_loop()
     // Path_Movement path = readPath();
     // Sensors sensors = readSensors();
 
-    Velocity2d cmd = controller.get_cmd(pose_state, path_queue);
+    Velocity2d cmd = trajectory_controller.get_cmd(pose_state, path_queue);
     send_velocity_command(cmd);
     /*motion_time += 1000 / hz;*/
     motion_time += 1 / hz;
