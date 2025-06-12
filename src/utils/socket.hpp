feca3bc602869a22f23598af6a34c6e1c9309d02
Dustin Shahan <echo@mailbox.org>
2025-06-12 15:30:10 +0200

    refactored to socket_parser

--- src/utils/socket.hpp
+++ src/utils/socket.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include "types.hpp"
+#include "socket_parser.hpp"
 #include <arpa/inet.h>
 #include <array>
 #include <queue>
@@ -9,12 +9,6 @@
 #include <vector>
 
 #define TCP_BUFFER_LENGTH 4096
-
-class Socket_Parser
-{
-  public:
-    virtual void push(std::queue<std::string> &msgs, std::span<const char> data) = 0;
-};
 
 class TCP_Socket
 {
@@ -34,29 +28,4 @@ class TCP_Socket
     std::vector<std::string> recv_all();
     std::string ublox_Message();
     int socket_fd = -1;
-};
-
-class NMEA_Parser : public Socket_Parser
-{
-  private:
-    Ring_Buffer<char, TCP_BUFFER_LENGTH * 2> ring;
-
-    inline bool skip_UBX();
-    bool extract_NMEA(std::string &out);
-    inline bool verify_Checksum(std::string_view s);
-    inline uint8_t hex_Val(char c);
-
-  public:
-    void push(std::queue<std::string> &msgs, std::span<const char> data) override;
-};
-
-class Ublox_JSON_Parser : public Socket_Parser
-{
-
-  private:
-    std::vector<char> buf;
-    Ring_Buffer<char, TCP_BUFFER_LENGTH * 4> ring;
-
-  public:
-    void push(std::queue<std::string> &msgs, std::span<const char> data) override;
 };
---
+++ src/utils/socket_parser.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "types.hpp"
+#include <queue>
+#include <string>
+#include <vector>
+
+class Socket_Parser
+{
+  public:
+    virtual void push(std::queue<std::string> &msgs, std::span<const char> data) = 0;
+};
+
+class NMEA_Parser : public Socket_Parser
+{
+  private:
+    Ring_Buffer<char, 8192> ring;
+
+    inline bool skip_UBX();
+    bool extract_NMEA(std::string &out);
+    inline bool verify_Checksum(std::string_view s);
+    inline uint8_t hex_Val(char c);
+
+  public:
+    void push(std::queue<std::string> &msgs, std::span<const char> data) override;
+};
+
+class Ublox_JSON_Parser : public Socket_Parser
+{
+
+  private:
+    std::vector<char> buf;
+    Ring_Buffer<char, 16384> ring;
+
+  public:
+    void push(std::queue<std::string> &msgs, std::span<const char> data) override;
+};
