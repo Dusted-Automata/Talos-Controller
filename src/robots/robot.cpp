83d4ca0331a437ac162e1c3d05cc47626e84f9ab
Dustin Shahan <echo@mailbox.org>
2025-06-20 20:11:11 +0200

    added a wait for heading to stabalize

--- src/robots/robot.cpp
+++ src/robots/robot.cpp
@@ -61,6 +61,20 @@ Robot::shutdown()
 bool
 Robot::init()
 {
+
     ublox.start();
+    std::cout << "ROBOT INIT!" << std::endl;
+    double heading_tolerance = 0.002;
+    while (true) {
+        std::optional<Nav_Att> msg = ublox.get_latest<Nav_Att>(Msg_Type::NAV_ATT);
+        if (msg.has_value()) {
+            if (msg.value().heading < heading_tolerance && msg.value().heading > -heading_tolerance) {
+                std::cout << "HEADING IS UNDER TOLERANCE!" << std::endl;
+                std::cout << "---------------------------" << std::endl;
+                break;
+            }
+        }
+    }
+    start();
     return true;
 }
