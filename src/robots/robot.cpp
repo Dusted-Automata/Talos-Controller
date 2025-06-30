b095cbca89de7694c51775fbe7b9d2fd5e589fec
Dustin Shahan <echo@mailbox.org>
2025-06-30 13:34:45 +0200

    better heading tolerance

--- src/robots/robot.cpp
+++ src/robots/robot.cpp
@@ -1,6 +1,7 @@
 #include "robot.hpp"
 #include "trajectory_controller.hpp"
 #include "types.hpp"
+#include <thread>
 
 // using namespace std::chrono;
 void
@@ -65,16 +66,20 @@ Robot::init()
     std::cout << "ROBOT INIT!" << std::endl;
     bool ublox_start = ublox.start();
     std::cout << "UBLOX: " << ublox_start << std::endl;
-    double heading_tolerance = 0.002;
+    double heading_tolerance = 0.5;
     while (true) {
         std::optional<Nav_Att> msg = ublox.get_latest<Nav_Att>(Msg_Type::NAV_ATT);
         if (msg.has_value()) {
-            if (msg.value().heading < heading_tolerance && msg.value().heading > -heading_tolerance) {
+            std::cout << "HEADING IS " << msg->heading << std::endl;
+            if (msg.value().heading < heading_tolerance || msg.value().heading > 360 - heading_tolerance) {
                 std::cout << "HEADING IS UNDER TOLERANCE!" << std::endl;
                 std::cout << "---------------------------" << std::endl;
                 break;
+            } else {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
             }
         }
     }
+    running = true;
     return true;
 }
