57b72bc5e08d3ab9ccb97602128fd44b749b0685
Dustin Shahan <echo@mailbox.org>
2025-07-31 22:47:07 +0200

    more experiments

--- src/control/linear_controller.cpp
+++ src/control/linear_controller.cpp
@@ -3,14 +3,17 @@
 #include "transformations.hpp"
 #include "types.hpp"
 #include <cmath>
+#include <iostream>
 
 Velocity2d
-Linear_Controller::get_cmd(Pose_State pose_state, Vector3d diff, double dt)
+Linear_Controller::get_cmd(Pose_State pose_state, Vector3d diff, Vector3d motion_profile_diff, double dt)
 {
     Velocity2d cmd = { .linear = Linear_Velocity().setZero(), .angular = Angular_Velocity().setZero() };
 
     double yaw_error = atan2(diff.y(), diff.x());
-    motion_profile.update(frames_dist(diff) - goal_tolerance_in_meters, dt);
+    std::cout << "yaw_error: " << yaw_error << std::endl;
+    // motion_profile.update(frames_dist(diff) - goal_tolerance_in_meters, dt);
+    motion_profile.update(frames_dist(motion_profile_diff) - goal_tolerance_in_meters, dt);
 
     cmd.linear.x() = motion_profile.velocity;
     cmd.linear.y() = 0.0;
