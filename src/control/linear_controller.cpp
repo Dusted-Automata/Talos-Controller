4f4f9d8ad75d5fa9ed383ae518df925b580c8e2c
Dustin Shahan <echo@mailbox.org>
2025-09-10 08:53:24 +0200

    added control logging

--- src/control/linear_controller.cpp
+++ src/control/linear_controller.cpp
@@ -5,6 +5,27 @@
 #include "ublox.hpp"
 #include <cmath>
 
+
+void log_data_plot(double sp, double heading_error, double control_output) {
+    FILE* file = fopen("../python/log.csv", "a"); // Open in append mode
+    if (!file) return;
+
+    fprintf(file, "%f,%f,%f\n", sp, heading_error, control_output);
+
+    fflush(file);   // ✅ flush buffer to disk
+    fclose(file);   // ✅ or just keep open and flush each time if speed matters
+}
+
+void log_data(double sp, double heading_error, double control_output) {
+    FILE* file = fopen("control.csv", "a"); // Open in append mode
+    if (!file) return;
+
+    fprintf(file, "%f,%f,%f\n", sp, heading_error, control_output);
+
+    fflush(file);   // ✅ flush buffer to disk
+    fclose(file);   // ✅ or just keep open and flush each time if speed matters
+}
+
 Velocity2d
 Linear_Controller::get_cmd(Pose_State pose_state, Vector3d diff, Vector3d motion_profile_diff, double dt)
 {
@@ -33,6 +54,16 @@ Linear_Controller::get_cmd(Pose_State pose_state, Vector3d diff, Vector3d motion
     // double yaw_error_offset = std::signbit(yaw_error) ? to_radian(-yaw_error_val) : to_radian(yaw_error_val);
     // cmd.angular_vel.z() = angular_pid.update(yaw_error_offset, -yaw_error, dt);
     cmd.angular_vel.z() = angular_pid.update(0, -yaw_error, dt);
+
+    //  if (header_file.is_open()) {
+    //     header_file << std::fixed;
+    //     header_file << 0 << ",";
+    //     header_file << yaw_error << ",";
+    //     header_file << cmd.angular_vel.z() << std::endl;
+    // }
+    log_data_plot(0, yaw_error, cmd.angular_vel.z());
+    log_data(0, yaw_error, cmd.angular_vel.z());
+    
 
     return cmd;
 }
